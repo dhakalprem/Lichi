cde2ebdfb7a99cb94722e2f29e6707fee757ca9f
Prem Prakash Dhakal <premprksh@hotmail.com>
2023-01-17 15:44:35 +0545

    Add project files.

---
+++ Lichi/Controllers/DesignationsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lichi.Model;
+
+namespace Lichi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DesignationsController : ControllerBase
+    {
+        private readonly LichiContext _context;
+
+        public DesignationsController(LichiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Designations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TblDesignation>>> GetTblDesignation()
+        {
+            return await _context.TblDesignation.ToListAsync();
+        }
+    }
+}
---
+++ Lichi/Model/LichiContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lichi.Model
+{
+    public class LichiContext : DbContext
+    {
+        public LichiContext(DbContextOptions<LichiContext> options) : base(options)
+        {
+        }
+        public DbSet<TblEmployee> TblEmployee { get; set; }
+        public DbSet<TblDesignation> TblDesignation { get; set; }
+    }
+}
